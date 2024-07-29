d0853b09a066b3bbcdf21147a69ad7a9091fef1f
halith-smh <mohamedhalith.smh@gmail.com>
2024-07-29 19:02:30 +0530

    View Cart Items Added

--- src/controllers/Cart.js
+++ src/controllers/Cart.js
@@ -35,6 +35,20 @@ const updateItems = async (req, res) => {};
 
 const removeItem = async (req, res) => {};
 
-const viewItems = async (req, res) => {};
+const viewItems = async (req, res) => {
+  const userId = req.userId;
+
+  try {
+    const user = await Users.findById(userId);
+
+    if (!user) {
+      return res.status(404).send(new ApiResponse(404, null, "User not found"));
+    }
+
+    res.status(200).send(new ApiResponse(200, user.cartItem, "Cart items"));
+  } catch (error) {
+    return res.status(404).send(new ApiResponse(500, null, "InternalError"));
+  }
+};
 
 module.exports = { addItems, removeItem, updateItems, viewItems };
